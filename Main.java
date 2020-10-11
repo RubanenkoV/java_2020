80869ccbb44ad8b45e30f6f6acc1b21a5a1b0d53
RubanenkoV <58641047+RubanenkoV@users.noreply.github.com>
2020-10-11 18:16:19 +0300

    HW 1 task 1.7 added

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*Программа находит произведение двух чисел и выводит результат в консоль*/
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    long a = in.nextLong();
+	    long b = in.nextLong();
+
+	    System.out.println(a * b);
+	    System.out.println("Рубаненко В.П. 16.09.2020, 14:56");
+		System.out.println("task 1.7");
+
+
+		}
+}
